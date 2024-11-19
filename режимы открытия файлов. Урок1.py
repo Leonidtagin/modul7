89086b8f4ee19cb58dbbacb26e0924a0329dca5e
Игорёха <leonidtagin401@gmail.com>
2024-11-19 21:12:04 +1100

    ...

--- режимы открытия файлов. Урок1.py
+++ режимы открытия файлов. Урок1.py
@@ -5,36 +5,35 @@ class Product:
         self.category = category
 
     def __str__(self):
-        return f'{self.name}, {self.weight}, {self.category}\n'
-
+        prods_str = str (f'{self.name}, {self.weight}, {self.category}')
+        return prods_str
 
 class Shop:
     def __init__(self):
-        self.products = []
+        self.__file_name = open('products.txt', 'a')
+        self.__file_name.close()
 
     def get_products(self):
-        file = open(self.__file_name, 'r')
-        data = file.read()
-        file.close()
-        return data
+        get_file = open('products.txt', 'r')
+        name_prod = get_file.read()
+        get_file.close()
+        return name_prod
 
     def add(self, *products):
-        for p in self.products:
-            if self.get_products().find(f'{p.name},') == -1:
-                print(f'Продукт {product.name} уже есть в магазине')
-                return self.products.appened(products)
-
-    def __str__(self):
-        return '\n'.join(str(p) for p in self.products)
+        for product in products:
+            if str(products) not in self.get_products():
+                file = open('products.txt', 'a+')
+                file.write(f'{str(product)}\n')
+                file.close()
+            else:
+                print(f'Продукт {product} уже есть в магазине')
 
 
 s1 = Shop()
 p1 = Product('Spaghetti', 3.4, 'Groceries')
 p2 = Product('Potato', 50.5, 'Vegetable')
 p3 = Product('Potato', 5.5, 'Vegetable')
+print(p2)
+s1.add(p1, p2, p3)
+print(s1.get_products())
 
-s1.add(p2)
-s1.add(p3)
-s1.add(p1)
-
-print(p1, p2, p3)
